1429f907f839fc2ff881733d07d1cfb2949f869c
alex1704 <kostenkoaleksander7@gmail.com>
2015-05-05 15:13:57 +0300

    Position now dependant from scrollview center but not from screen center

--- Class/CBStoreHouseRefreshControl.m
+++ Class/CBStoreHouseRefreshControl.m
@@ -137,7 +137,7 @@ NSString *const yKey = @"y";
 - (void)scrollViewDidScroll
 {
     if (self.originalTopContentInset == 0) self.originalTopContentInset = self.scrollView.contentInset.top;
-    self.center = CGPointMake([UIScreen mainScreen].bounds.size.width/2, self.realContentOffsetY*krelativeHeightFactor);
+    self.center = CGPointMake(self.scrollView.frame.size.width * 0.5, self.realContentOffsetY*krelativeHeightFactor);
     if (self.state == CBStoreHouseRefreshControlStateIdle)
         [self updateBarItemsWithProgress:self.animationProgress];
 }
